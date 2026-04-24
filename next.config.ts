59b2eb215e1793d438270bc9311385376c546f07
shahriyor <nnniggga457@gmail.com>
2026-04-24 17:55:19 +0500

    Fix image base path resolution

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: basePath,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  }
 };
 
 export default nextConfig;
