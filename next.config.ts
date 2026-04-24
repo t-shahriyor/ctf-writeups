e98e87ebae234807ed8c0531fa348d3e8fdd9cb6
shahriyor <nnniggga457@gmail.com>
2026-04-24 14:50:17 +0500

    Fix github pages CSS paths

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = '';
+
+if (isGithubActions) {
+  // e.g. "USERNAME/REPO_NAME"
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+  if (repo) {
+    basePath = `/${repo}`;
+  }
+}
+
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: basePath,
 };
 
 export default nextConfig;
